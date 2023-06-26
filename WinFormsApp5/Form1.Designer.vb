ccd7c4981df4c92f497853f269ef9e1a61fc91a0
Mahadevii <mshadrina@gmail.com>
2023-06-26 06:20:25 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button6 = New Button()
+        Button8 = New Button()
+        DataGridView1 = New DataGridView()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        PrintDocument1 = New Printing.PrintDocument()
+        PrintDialog1 = New PrintDialog()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(17, 11)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(104, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(127, 12)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(127, 111)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 2
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(227, 709)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(115, 29)
+        Button6.TabIndex = 5
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(1275, 709)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(115, 29)
+        Button8.TabIndex = 7
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(227, 11)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 12
+        DataGridView1.Size = New Size(1163, 692)
+        DataGridView1.TabIndex = 8
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(17, 80)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(49, 27)
+        TextBox1.TabIndex = 9
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(17, 113)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(49, 27)
+        TextBox2.TabIndex = 10
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(17, 146)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(49, 27)
+        TextBox3.TabIndex = 11
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(18, 51)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 12
+        Label1.Text = "Label1"' 
+        ' PrintDocument1
+        ' 
+        ' 
+        ' PrintDialog1
+        ' 
+        PrintDialog1.UseEXDialog = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1423, 750)
+        Controls.Add(Label1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button8)
+        Controls.Add(Button6)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintDialog1 As PrintDialog
+End Class
---
+++ WinFormsApp5/Form1.vb
@@ -0,0 +1,149 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
+
+Public Class Form1
+    Dim alf As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    Dim dt As New DataTable ' — создание таблицы данных
+    Dim ds As New DataSet ' — создание набора данных
+    Dim column As Integer = 0
+    Dim cicle As Integer = 0
+    Dim matrixM As String = ""
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label1.Text = "Размерность матриц:"
+        Button1.Text = "Добавить столбец"
+        Button2.Text = "Добавить строку"
+        Button3.Text = "Заполнить матрицы"
+        Button6.Text = "Добавить 25*25"
+        Button8.Text = "Сохранить"
+        If IO.File.Exists("C:\Users\Shadrina Maria\Desktop\tabl.xml") = False Then
+            ' Если XML-файла НЕТ:
+            DataGridView1.DataSource = dt
+            'Добавить объект dt в DataSet:
+            ds.Tables.Add(dt)
+        Else 'Если XML-файл ЕCТЬ:
+            ds.ReadXml("C:\Users\Shadrina Maria\Desktop\tabl.xml")
+            DataGridView1.DataMember = "Элемент"
+            DataGridView1.DataSource = ds
+        End If
+    End Sub
+    Private Sub Write(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        dt.TableName = "Название таблицы"
+        ds.WriteXml("C:\Users\Shadrina Maria\Desktop\tabl.xml")
+    End Sub
+    Private Sub AddColumn(sender As Object, e As EventArgs) Handles Button1.Click
+        If column = 26 Then
+            cicle += 1
+            column = 0
+        Else
+            If cicle = 0 Then
+                dt.Columns.Add(alf(column))
+            Else
+                dt.Columns.Add(alf(column) + Str(cicle))
+            End If
+            column = column + 1
+        End If
+    End Sub
+    Private Sub AddRow(sender As Object, e As EventArgs) Handles Button2.Click
+        dt.Rows.Add()
+    End Sub
+
+    Private Sub Eve(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim nM1 = CInt(TextBox1.Text)
+        Dim nM2 = CInt(TextBox2.Text)
+        Dim nM3 = CInt(TextBox3.Text)
+
+        DataGridView1.Rows(0).Cells(0).Value = "Матрица A:"
+        FillRandom(0, nM1 - 1, 1, nM1, nM1)
+        Dim maxA = MaxNormMatrix(0, nM1 - 1, 1, nM1, nM1)
+        DataGridView1.Rows(0).Cells(1).Value = "Максимум = " + Str(maxA)
+
+        DataGridView1.Rows(nM1 + 1).Cells(0).Value = "Матрица B:"
+        FillRandom(0, nM2 - 1, nM1 + 2, nM1 + nM2 + 1, nM2)
+        Dim maxB = MaxNormMatrix(0, nM2 - 1, nM1 + 2, nM1 + nM2 + 1, nM2)
+        DataGridView1.Rows(nM1 + 1).Cells(1).Value = "Максимум = " + Str(maxB)
+
+        DataGridView1.Rows(nM1 + nM2 + 2).Cells(0).Value = "Матрица C:"
+        FillRandom(0, nM3 - 1, nM1 + nM2 + 3, nM1 + nM2 + nM3 + 2, nM3)
+        Dim maxC = MaxNormMatrix(0, nM3 - 1, nM1 + nM2 + 3, nM1 + nM2 + nM3 + 2, nM3)
+        DataGridView1.Rows(nM1 + nM2 + 2).Cells(1).Value = "Максимум = " + Str(maxC)
+
+
+
+        If maxA <= maxB And maxA <= maxC Then
+            matrixM = MatrixStr(0, nM1 - 1, 1, nM1)
+        ElseIf maxB <= maxA And maxB <= maxC Then
+            matrixM = MatrixStr(0, nM2 - 1, nM1 + 2, nM1 + nM2 + 1)
+        ElseIf maxC <= maxA And maxC <= maxB Then
+            matrixM = MatrixStr(0, nM3 - 1, nM1 + nM2 + 3, nM1 + nM2 + nM3 + 2)
+        End If
+
+        PrintDialog1.Document = PrintDocument1
+        If (PrintDialog1.ShowDialog() = DialogResult.OK) Then
+            PrintDocument1.Print()
+        End If
+    End Sub
+    Private Sub FillRandom(col1 As Integer, col2 As Integer, row1 As Integer, row2 As Integer, stepAbs As Integer)
+        Randomize()
+        For i = row1 To row2
+            For j = col1 To col2
+                Dim randomValue = CInt(-200 * Rnd() + 100)
+                DataGridView1.Rows(i).Cells(j).Value = randomValue
+                DataGridView1.Rows(i).Cells(j + stepAbs).Value = Math.Abs(randomValue)
+                DataGridView1.Rows(i).Cells(j + stepAbs).Style.BackColor = Color.LightGray
+            Next j
+        Next i
+    End Sub
+    Private Function MaxNormMatrix(col1 As Integer, col2 As Integer, row1 As Integer, row2 As Integer, stepAbs As Integer) As Integer
+        Dim maxCount = DataGridView1.Rows(row1).Cells(col1 + stepAbs).Value
+        For i = row1 To row2
+            For j = col1 To col2
+                Dim n = DataGridView1.Rows(i).Cells(j + stepAbs).Value
+                If n > maxCount Then
+                    maxCount = n
+                End If
+            Next j
+        Next i
+        'Закрашивание клетки с максимумом матрицы
+        For i = row1 To row2
+            For j = col1 To col2
+                If DataGridView1.Rows(i).Cells(j + stepAbs).Value = maxCount Then
+                    DataGridView1.Rows(i).Cells(j + stepAbs).Style.BackColor = Color.LightGreen
+                End If
+            Next j
+        Next i
+        Return maxCount
+    End Function
+
+    Private Function MatrixStr(col1 As Integer, col2 As Integer, row1 As Integer, row2 As Integer) As String
+        For i = row1 To row2
+            For j = col1 To col2
+                matrixM += Str(DataGridView1.Rows(i).Cells(j).Value) + " "
+            Next j
+            matrixM += vbCrLf
+        Next i
+        Return matrixM
+    End Function
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        For i = 0 To 25
+            dt.Rows.Add()
+            If column = 26 Then
+                cicle += 1
+                column = 0
+            Else
+                If cicle = 0 Then
+                    dt.Columns.Add(alf(column))
+                Else
+                    dt.Columns.Add(alf(column) + Str(cicle))
+                End If
+                column = column + 1
+            End If
+        Next i
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim printFont = New Font("Times New Roman", 14)
+        e.Graphics.DrawString(matrixM, printFont, Brushes.Black, e.MarginBounds, New StringFormat())
+        e.HasMorePages = False
+    End Sub
+End Class
